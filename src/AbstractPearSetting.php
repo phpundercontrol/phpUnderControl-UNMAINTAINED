cbbb1ac217f4383d506079336bd3ee8cd6c2813b
Manuel Pichler <mapi@manuel-pichler.de>
2007-11-19 10:45:02 +0000

    - License changed to "New BSD"
    git-svn-id: svn://phpunit.de/phpunit/phpUnderControl/trunk@1659 c4a080f7-5c17-0410-a942-8af0f0140c7b

--- src/AbstractPearSetting.php
+++ src/AbstractPearSetting.php
@@ -2,19 +2,37 @@
 /**
  * This file is part of phpUnderControl.
  *
- * phpUnderControl is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright (c) 2007, Manuel Pichler <mapi@manuel-pichler.de>.
+ * All rights reserved.
  *
- * phpUnderControl is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * You should have received a copy of the GNU General Public License
- * along with phpUnderControl; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * 
  * @package phpUnderControl
  */
@@ -25,7 +43,7 @@
  * @package   phpUnderControl
  * @author    Manuel Pichler <mapi@manuel-pichler.de>
  * @copyright 2007 Manuel Pichler. All rights reserved.
- * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version   $Id$
  * 
  * @property      string $cliTool        The PEAR cli command line tool.
