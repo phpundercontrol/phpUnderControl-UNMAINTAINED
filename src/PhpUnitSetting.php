38bfd1b5da9a1cffdc0d2fed2a7af5cca5bf3ac2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2007-11-21 14:56:09 +0000

    --report -> --coverage-html
    git-svn-id: svn://phpunit.de/phpunit/phpUnderControl/trunk@1688 c4a080f7-5c17-0410-a942-8af0f0140c7b

--- src/PhpUnitSetting.php
+++ src/PhpUnitSetting.php
@@ -85,8 +85,8 @@ class pucPhpUnitSetting extends pucAbstractPearSetting
         $coverage = '';
         if ( $this->coverage === true )
         {
-            $coverage = '--coverage-xml ${basedir}/build/logs/phpunit.coverage.xml
-                         --report ${basedir}/build/coverage';
+            $coverage = '--coverage-xml  ${basedir}/build/logs/phpunit.coverage.xml
+                         --coverage-html ${basedir}/build/coverage';
         }
         $output = '';
         if ( $this->outputDir !== null )
