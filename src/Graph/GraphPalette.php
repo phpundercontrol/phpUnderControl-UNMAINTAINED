8a27a4bcf95fe7df91acf6194de2e57a9a74d519
Manuel Pichler <mapi@manuel-pichler.de>
2008-01-13 01:07:45 +0000

    - New ezcGraph based metrics stuff started.
    git-svn-id: svn://phpunit.de/phpunit/phpUnderControl/trunk@2068 c4a080f7-5c17-0410-a942-8af0f0140c7b

---
+++ src/Graph/GraphPalette.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * This file is part of phpUnderControl.
+ * 
+ * PHP Version 5.2.4
+ *
+ * Copyright (c) 2007-2008, Manuel Pichler <mapi@manuel-pichler.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * @category  QualityAssurance
+ * @package   Graph
+ * @author    Manuel Pichler <mapi@manuel-pichler.de>
+ * @copyright 2007-2008 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://www.phpundercontrol.org/
+ */
+
+/**
+ * Customized ezcGraph palette with typical phpUnderControl look.
+ *
+ * @category  QualityAssurance
+ * @package   Graph
+ * @author    Manuel Pichler <mapi@manuel-pichler.de>
+ * @copyright 2007-2008 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://www.phpundercontrol.org/
+ */
+class phpucGraphPalette extends ezcGraphPaletteEzGreen
+{
+    /**
+     * Tango white background color.
+     *
+     * @type string
+     * @var string $background
+     */
+    protected $background = '#eeeeef';
+
+    /**
+     * Light gray grid lines.
+     *
+     * @type string
+     * @var string $majorGridColor
+     */
+    protected $majorGridColor = '#d3d7cf';
+    
+    /**
+     * Four gray and green tango colors for different graph records.
+     *
+     * @type array<string>
+     * @var array(string) $dataSetColor
+     */
+    protected $dataSetColor = array(
+        '#8ae234',
+        '#437610',
+        '#888a85',
+        '#2e3436',
+    );
+
+    /**
+     * By default no symbol used.
+     *
+     * @type array<integer>
+     * @var array(integer) $dataSetSymbol
+     */
+    protected $dataSetSymbol = array(
+        ezcGraph::NO_SYMBOL,
+    );
+
+    /**
+     * Sans-serif graph font.
+     *
+     * @type string
+     * @var string $fontName
+     */
+    protected $fontName = 'sans-serif';
+
+    /**
+     * Tango gray text.
+     *
+     * @type string
+     * @var string $fontColor
+     */
+    protected $fontColor = '#555753';
+}
