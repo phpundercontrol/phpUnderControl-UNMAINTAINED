336aae2662542e7b647b68babfa9a4c96c8be858
Manuel Pichler <mapi@manuel-pichler.de>
2008-02-22 17:51:30 +0000

    - Subversion checkout implementation started.
    git-svn-id: svn://phpunit.de/phpunit/phpUnderControl/trunk@2517 c4a080f7-5c17-0410-a942-8af0f0140c7b

---
+++ src/VersionControl/SubversionCheckout.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * This file is part of phpUnderControl.
+ * 
+ * PHP Version 5.2.4
+ *
+ * Copyright (c) 2007-2008, Manuel Pichler <mapi@phpundercontrol.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * @category  QualityAssurance
+ * @package   VersionControl
+ * @author    Manuel Pichler <mapi@phpundercontrol.org>
+ * @copyright 2007-2008 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://www.phpundercontrol.org/
+ */
+
+/**
+ * Subversion checkout implementation. 
+ *
+ * @category  QualityAssurance
+ * @package   VersionControl
+ * @author    Manuel Pichler <mapi@phpundercontrol.org>
+ * @copyright 2007-2008 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://www.phpundercontrol.org/
+ * 
+ * @property string $url
+ *           The subversion repository url.
+ * @property string $dest
+ *           The checkout destination directory.
+ */
+class phpucSubversionCheckout
+{
+    /**
+     * Virtual properties for the setting implementation.
+     *
+     * @type array<mixed>
+     * @var array(string=>mixed) $properties
+     */
+    protected $properties = array(
+        'url'   =>  null,
+        'dest'  =>  null
+    );
+    
+    /**
+     * Performs a subversion checkout.
+     *
+     * @return void
+     */
+    public function checkout()
+    {
+        $svn = phpucFileUtil::findExecutable( 'svn' );
+        $cmd = escapeshellcmd( "{$svn} co {$this->url} {$this->dest}" );
+
+        exec( $cmd );
+    }
+    
+    /**
+     * Magic property isset method.
+     *
+     * @param string $name The property name.
+     * 
+     * @return boolean
+     */
+    public function __isset( $name )
+    {
+        return array_key_exists( $name, $this->properties );
+    }
+    
+    /**
+     * Magic property getter method.
+     *
+     * @param string $name The property name.
+     * 
+     * @return mixed
+     * @throws OutOfRangeException If the property doesn't exist or is writonly.
+     */
+    public function __get( $name )
+    {
+        if ( array_key_exists( $name, $this->properties ) === true )
+        {
+            return $this->properties[$name];
+        }
+        throw new OutOfRangeException( 
+            sprintf( 'Unknown or writonly property $%s.', $name )
+        );
+    }
+    
+    /**
+     * Magic property setter method.
+     *
+     * @param string $name  The property name.
+     * @param mixed  $value The property value.
+     * 
+     * @return void
+     * @throws OutOfRangeException If the property doesn't exist or is readonly.
+     */
+    public function __set( $name, $value )
+    {
+        switch ( $name )
+        {
+            case 'url':
+            case 'dest':
+                $this->properties[$name] = $value;
+                break;
+                
+            default:
+                throw new OutOfRangeException(
+                    sprintf( 'Unknown or readonly property $%s.', $name )
+                );
+                break;                
+        }
+
+    }
+}
